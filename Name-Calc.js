55b92d63419df190c31325b43651b60b39fd4d26
TomKat_Ivyb-pc <taitan.tomoaki@gmail.com>
2021-02-08 06:31:50 +0900

    tab追加で階層を修正

--- Name-Calc.js
+++ Name-Calc.js
@@ -17,61 +17,73 @@ var hantei = 0;
 var parent = "";
 
 function CheckCalc() {
-console.log("■■■計算開始■■■");
+    console.log("■■■計算開始■■■");
 
-//2度目以降のボタン押下時に、以前の要素・カウントを削除する
-if (document.querySelector('div p') != null) {
-    console.log('div-p');
-    parent = document.getElementById('four');
-    parent.removeChild(parent.firstChild);
-    parent = document.getElementById('five');
-    parent.removeChild(parent.firstChild);
-    cnt_FullName_Boin = 0;
-    cnt_FullName_Shiin = 0;
-}
+    //2度目以降のボタン押下時に、以前の要素・カウントを削除する
+    if (document.querySelector('div p') != null) {
+        console.log('div-p');
+        parent = document.getElementById('four');
+        parent.removeChild(parent.firstChild);
+        parent = document.getElementById('five');
+        parent.removeChild(parent.firstChild);
+        cnt_FullName_Boin = 0;
+        cnt_FullName_Shiin = 0;
+    }
 
-/* 苗字の判定 */
-if (document.nyuryoku.myouji.value == "") {
-    console.log("空欄");
-} else {
-    var txtFullName = document.nyuryoku.myouji.value;
-    console.log('入力した名前は：' + txtFullName + '　です');
+    /* 苗字の判定 */
+    if (document.nyuryoku.myouji.value == "") {
+        console.log("空欄");
+    } else {
+        var txtFullName = document.nyuryoku.myouji.value;
+        console.log('入力した名前は：' + txtFullName + '　です');
 
-    /* 一文字づつ抜き出し */
-    for (let i = 0; i < txtFullName.length; i++) {
-        hantei = 0;
-        // console.log('名前のi文字目=' + (i + 1) + ':' + txtFullName[i]);
+        /* 一文字づつ抜き出し */
+        for (let i = 0; i < txtFullName.length; i++) {
+            hantei = 0;
+            // console.log('名前のi文字目=' + (i + 1) + ':' + txtFullName[i]);
 
-        /* リストの個数分forをまわす */
-        for (let j = 1; j <= 9; j++) {
-            li_moji_x = eval("li_moji_" + j);
-            // console.log(li_moji_x);
+            /* リストの個数分forをまわす */
+            for (let j = 1; j <= 9; j++) {
+                li_moji_x = eval("li_moji_" + j);
+                // console.log(li_moji_x);
 
-            /* リスト内要素分forをまわす(3回、１要素目は数字を取得する目的のためforの除外) */
-            for (let k = 1; k <= 3; k++) {
-                // console.log('リスト:' + li_moji_x + ' K:' + k);
-                // console.log('要素から抜き出した文字' + li_moji_x[k]);
-                
-                /* 抜き出し文字が"k","v"は11,22でカウント */
-                if (txtFullName[i] == "k") {
-                    console.log(txtFullName[i]);
-                    cnt_FullName_Shiin += 11
-                    hantei = 1;
-                    break;
-                }
-                if (txtFullName[i] == "v") {
-                    console.log(txtFullName[i]);
-                    cnt_FullName_Shiin += 22
-                    hantei = 1;
-                    break;
-                }
+                /* リスト内要素分forをまわす(3回、１要素目は数字を取得する目的のためforの除外) */
+                for (let k = 1; k <= 3; k++) {
+                    // console.log('リスト:' + li_moji_x + ' K:' + k);
+                    // console.log('要素から抜き出した文字' + li_moji_x[k]);
+                    
+                    /* 抜き出し文字が"k","v"は11,22でカウント */
+                    if (txtFullName[i] == "k") {
+                        console.log(txtFullName[i]);
+                        cnt_FullName_Shiin += 11
+                        hantei = 1;
+                        break;
+                    }
+                    if (txtFullName[i] == "v") {
+                        console.log(txtFullName[i]);
+                        cnt_FullName_Shiin += 22
+                        hantei = 1;
+                        break;
+                    }
 
-                /* リスト内要素が母音である場合、カウント */
-                for (let l = 0; l < li_boin.length; l++) {
-                    // console.log("抜き出した母音" + li_boin[l]);
-                    if (txtFullName[i] == li_moji_x[k] && txtFullName[i] == li_boin[l]) {
-                        cnt_FullName_Boin += li_moji_x[0]
-                        // console.log('カウント(母音)：' + cnt_FullName_Boin);
+                    /* リスト内要素が母音である場合、カウント */
+                    for (let l = 0; l < li_boin.length; l++) {
+                        // console.log("抜き出した母音" + li_boin[l]);
+                        if (txtFullName[i] == li_moji_x[k] && txtFullName[i] == li_boin[l]) {
+                            cnt_FullName_Boin += li_moji_x[0]
+                            // console.log('カウント(母音)：' + cnt_FullName_Boin);
+                            hantei = 1;
+                            break;
+                        }
+                    }
+                    if (hantei == 1) {
+                        break;
+                    }
+
+                    /* 抜き出し文字がリスト内要素と同じ文字である場合 */
+                    if (txtFullName[i] == li_moji_x[k] && hantei != 1) {
+                        cnt_FullName_Shiin += li_moji_x[0]
+                        // console.log('カウント(子音)：' + cnt_FullName_Shiin);
                         hantei = 1;
                         break;
                     }
@@ -79,28 +91,16 @@ if (document.nyuryoku.myouji.value == "") {
                 if (hantei == 1) {
                     break;
                 }
-
-                /* 抜き出し文字がリスト内要素と同じ文字である場合 */
-                if (txtFullName[i] == li_moji_x[k] && hantei != 1) {
-                    cnt_FullName_Shiin += li_moji_x[0]
-                    // console.log('カウント(子音)：' + cnt_FullName_Shiin);
-                    hantei = 1;
-                    break;
-                }
-            }
-            if (hantei == 1) {
-                break;
             }
         }
     }
-}
-var doc = document.createElement("p");
-doc.innerText = 'カウント(子音)：' + cnt_FullName_Shiin;
-var doc_div = document.getElementById("four");
-doc_div.appendChild(doc);
+    var doc = document.createElement("p");
+    doc.innerText = 'カウント(子音)：' + cnt_FullName_Shiin;
+    var doc_div = document.getElementById("four");
+    doc_div.appendChild(doc);
 
-var doc = document.createElement("p");
-doc.innerText = 'aiueo(母音)：' + cnt_FullName_Boin;
-var doc_div = document.getElementById("five");
-doc_div.appendChild(doc);
+    var doc = document.createElement("p");
+    doc.innerText = 'aiueo(母音)：' + cnt_FullName_Boin;
+    var doc_div = document.getElementById("five");
+    doc_div.appendChild(doc);
 }
